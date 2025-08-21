0cee5378bc9ffde76ca10c0645b8d332cec0d166
William Vilas Boas Gomes <willzgomes@gmail.com>
2025-08-21 20:53:33 -0300

    feat(auth): add refresh token guard for GET /api/auth/profile endpoint

---
+++ src/auth/guards/refresh-token.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class RefreshTokenGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const token = request.cookies['refresh_token'];
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
