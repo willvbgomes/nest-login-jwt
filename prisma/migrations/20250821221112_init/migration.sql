6d8c71bd163215ef228e4fa9b5cfd32b61f67bd2
William Vilas Boas Gomes <willzgomes@gmail.com>
2025-08-21 19:43:45 -0300

    chore(database): update prisma migrations

---
+++ prisma/migrations/20250821221112_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
