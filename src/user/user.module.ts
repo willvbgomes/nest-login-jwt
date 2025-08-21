b783c946eeab62c050cfd5d448d4bc7eb2f00811
William Vilas Boas Gomes <willzgomes@gmail.com>
2025-08-21 19:17:33 -0300

    refactor(user): fix missing export of user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  exports: [UserService],
   imports: [DatabaseModule],
   providers: [UserService],
 })
