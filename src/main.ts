4935369db83ceb5cb55e0f0e0fe541fc486cac69
William Vilas Boas Gomes <willzgomes@gmail.com>
2025-08-21 23:02:02 -0300

    chore(config): setup cookie parser and cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
